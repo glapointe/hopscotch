8ffdfdf2b101a1aeb6d024627cc8754e635bc566
Gary Lapointe <gary@thelapointes.com>
2022-05-06 10:37:08 -0400

    Changed scroll into view logic.

--- dist/js/hopscotch.js
+++ dist/js/hopscotch.js
@@ -53,7 +53,56 @@
     left: 'right',
     right: 'left'
   };
+  if (!Element.prototype.scrollIntoViewIfNeeded) {
+    Element.prototype.scrollIntoViewIfNeeded = function (centerIfNeeded) {
+      "use strict";
 
+      function makeRange(start, length) {
+        return { "start": start, "length": length, "end": start + length };
+      }
+
+      function coverRange(inner, outer) {
+        if (false === centerIfNeeded || outer.start < inner.end && inner.start < outer.end) {
+          return Math.max(inner.end - outer.length, Math.min(outer.start, inner.start));
+        }
+        return (inner.start + inner.end - outer.length) / 2;
+      }
+
+      function makePoint(x, y) {
+        return {
+          "x": x,
+          "y": y,
+          "translate": function translate(dX, dY) {
+            return makePoint(x + dX, y + dY);
+          }
+        };
+      }
+
+      function absolute(elem, pt) {
+        while (elem) {
+          pt = pt.translate(elem.offsetLeft, elem.offsetTop);
+          elem = elem.offsetParent;
+        }
+        return pt;
+      }
+
+      var target = absolute(this, makePoint(0, 0)),
+          extent = makePoint(this.offsetWidth, this.offsetHeight),
+          elem = this.parentNode,
+          origin;
+
+      while (elem instanceof HTMLElement) {
+        // Apply desired scroll amount.
+        origin = absolute(elem, makePoint(elem.clientLeft, elem.clientTop));
+        elem.scrollLeft = coverRange(makeRange(target.x - origin.x, extent.x), makeRange(elem.scrollLeft, elem.clientWidth));
+        elem.scrollTop = coverRange(makeRange(target.y - origin.y, extent.y), makeRange(elem.scrollTop, elem.clientHeight));
+
+        // Determine actual scroll amount by reading back scroll properties.
+        target = target.translate(-elem.scrollLeft, -elem.scrollTop);
+        elem = elem.parentNode;
+      }
+    };
+  }
   // If cookies are disabled, accessing sessionStorage can throw an error.
   // sessionStorage could also throw an error in Safari on write (even though it exists).
   // So, we'll try writing to sessionStorage to verify it's available.
@@ -1682,7 +1731,8 @@
       }
       var scrollableParent = utils.getScrollParent(targetEl, false);
       if (scrollableParent != null && scrollableParent !== document) {
-        targetEl.scrollIntoView({ behavior: 'smooth' });
+        //   targetEl.scrollIntoView({behavior:'smooth'});
+        targetEl.scrollIntoViewIfNeeded(true);
         var elTop = targetEl.getBoundingClientRect().top;
         var interval = setInterval(function () {
           // Don't need this now as it's being handled by the onscroll event
